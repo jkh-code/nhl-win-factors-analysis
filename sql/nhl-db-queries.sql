520a0788799cee9a3f2aba20ca20c81dca200024
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2021-06-08 09:42:39 -0700

    Complete python sql query

---
+++ sql/nhl-db-queries.sql
@@ -0,0 +1,8 @@
+SELECT
+    season, team, game, gp, wins, losses, ot_losses, points,
+    point_percent, reg_wins, reg_ot_wins, so_wins, gf, ga, 
+    gf_per_gp, ga_per_gp, pp_percent, pk_percent, pp_net_percent, 
+    pk_net_percent, sf_per_gp, sa_per_gp, fo_win_percent
+FROM games
+WHERE season BETWEEN 2009 AND 2018
+;
