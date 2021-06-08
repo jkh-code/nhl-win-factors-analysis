8d2ec1d38218e134f2dfe5c48ca9931de5ec66f3
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2021-06-08 16:00:31 -0700

    Add check for ties query

--- sql/nhl-db-queries.sql
+++ sql/nhl-db-queries.sql
@@ -1,3 +1,4 @@
+-- Python query
 SELECT
     season, team, game, gp, wins, losses, ot_losses, points,
     point_percent, reg_wins, reg_ot_wins, so_wins, gf, ga, 
@@ -5,4 +6,12 @@ SELECT
     pk_net_percent, sf_per_gp, sa_per_gp, fo_win_percent
 FROM games
 WHERE season BETWEEN 2009 AND 2018
+;
+
+-- Checking ties
+-- It looks like there are no ties
+SELECT *
+FROM games
+WHERE season BETWEEN 2009 AND 2018
+    AND ties IS NOT NULL
 ;
