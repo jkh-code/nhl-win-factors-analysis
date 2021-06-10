f7947c567a7704f64a423ed3581864b2794a4cfa
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2021-06-09 17:25:49 -0700

    Create create plots script

---
+++ src/create_plots.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from web_scraping import make_postgres_conn
+
+pd.set_option('display.max_rows', 100)
+plt.style.use('ggplot')
+
+
+if __name__ == '__main__':
+    pass
